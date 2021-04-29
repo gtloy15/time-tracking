b960187680a03ab3a6186075bef56a6be2c691ff
Jesse Rurka <jdrurka@liberty.edu>
2021-04-29 13:28:13 -0400

    Python script to track time automatically

---
+++ recordTimeWorked.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pandas as PD
+
+columns = ['Description', 'Start date', 'Duration', 'Tags']
+source_info = PD.read_csv('time-worked.csv', usecols = columns)
+source_info.rename(columns={'Start date': 'Date'}, inplace=True)
+
+url = r'https://liberty.service-now.com/task_time_worked.do?sys_id=-1&sysparm_stack=task_time_worked_list.do'
+path = r'C:\dev\drivers\microsoft-edge-driver\msedgedriver.exe'
+
+browser = webdriver.Edge(path)
+browser.get(url)
+wait = WebDriverWait(browser, 10)
+time.sleep(3)
+
+def fill_form(description, date, duration, tags):
+    #initialize url
+    browser.get(url)
+    WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.ID, 'sysverb_insert_bottom')))
+    #find elements
+    in_comments = browser.find_element_by_id('task_time_worked.comments')
+    in_time_hours = browser.find_element_by_id('ni.task_time_worked.time_workeddur_hour')
+    in_time_min = browser.find_element_by_id('ni.task_time_worked.time_workeddur_min')
+    in_date = browser.find_element_by_id('task_time_worked.u_created_for')
+    in_category = Select(browser.find_element_by_id('task_time_worked.u_category'))
+    in_task = browser.find_element_by_id('sys_display.task_time_worked.task')
+    submit = browser.find_element_by_id('sysverb_insert_bottom')
+    #input data
+    in_task.clear()
+    if (tags == 'pd'):
+        in_category.select_by_index(2)
+    elif (tags == 'ooo'):
+        in_category.select_by_index(1)
+    elif (tags == 'conv'):
+        in_category.select_by_index(3)
+    else:
+        in_task.send_keys(tags)
+    time.sleep(1)
+    in_date.clear()
+    in_date.send_keys(date)
+    time.sleep(1)
+    in_time_hours.clear()
+    in_time_hours.send_keys(duration[0])
+    time.sleep(1)
+    in_time_min.clear()
+    in_time_min.send_keys(duration[2:4])
+    time.sleep(1)
+    in_comments.clear()
+    in_comments.send_keys(description)
+    time.sleep(1)
+    #submit
+    #submit.click()
+    time.sleep(5)
+
+for row in source_info.itertuples():
+    fill_form(row.Description, row.Date, row.Duration, row.Tags)
+
+print("Procedure Completed.")
